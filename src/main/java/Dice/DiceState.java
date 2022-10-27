1ab029df352ea8627ea42644a2a1561d2a4c08ad
Navaty Khara <navatykhara@gmail.com>
2022-10-27 15:23:04 -0400

    CODE for testCoinStateChar() for testing char output

--- src/main/java/Dice/DiceState.java
+++ src/main/java/Dice/DiceState.java
@@ -14,7 +14,9 @@ public enum DiceState {
         public String toString(){
             return "COIN";
         }
-
+        public char toChar(){
+            return 'C';
+        }
     }
 
     ;
