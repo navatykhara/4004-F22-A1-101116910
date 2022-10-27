498aad8d446058fa924773cc513df2c4efa8cf19
Navaty Khara <navatykhara@gmail.com>
2022-10-27 16:56:05 -0400

    CODE for implementing constructor and constructor extension. They are seperated for legibility and ease of testing.

---
+++ src/main/java/Dice/Hand.java
@@ -0,0 +1,26 @@
+package Dice;
+
+import java.util.ArrayList;
+
+public class Hand {
+
+    final int MAX_DICE = 8;
+
+    Dice[] hand;
+    ArrayList<Dice> graveyard;
+
+    public Hand(){
+        hand = new Dice[MAX_DICE];
+        graveyard = new ArrayList<>();
+    }
+
+    public void initialize(){
+        for(int i = 0; i < MAX_DICE; i++){
+            hand[i] = new Dice();
+            hand[i].roll();
+        }
+
+        graveyard = new ArrayList<>();
+    }
+
+}
