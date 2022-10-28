532cb3577b9c3f6335fa1cd7954d9df2e6f6d351
Navaty Khara <navatykhara@gmail.com>
2022-10-27 21:11:38 -0400

    CODE for implementation of getHand and hand variables for TREASURECHEST

---
+++ src/main/java/FortuneCards/TreasureChest.java
@@ -0,0 +1,13 @@
+package FortuneCards;
+
+import Dice.Hand;
+
+public class TreasureChest implements  FortuneCards{
+
+    Hand chest = new Hand();
+
+    public Hand getHand(){
+        return chest;
+    }
+    
+}
