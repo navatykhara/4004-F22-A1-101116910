808106ccdf1eacfdeb5087fa67f01d13e4d805fc
Navaty Khara <navatykhara@gmail.com>
2022-10-27 23:27:28 -0400

    U-TEST for testing non-rigged roll function of Hand. Rigged function is setHand. Non-rigged has randomness but there are boundaries we can testnamely out of bounds inputs

--- src/test/java/Dice/HandTest.java
+++ src/test/java/Dice/HandTest.java
@@ -223,5 +223,46 @@ class HandTest {
         assertTrue(true);
 
     }
+    @Test
+    void rollAllOutofBounds(){
+
+        Hand hand = new Hand();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.COIN);
+        d2.setDice(DiceState.COIN);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.COIN);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.COIN);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.COIN);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        hand.cleanUp();
+        hand.roll(new int[] {8, 8, 8, 8, 8, 8, 8, 8});
+
+        for(int i = 0; i < hand.getHand().length; i++)
+            if(hand.getHand()[i].toString().equals(DiceState.COIN.toString()))
+                assertTrue(false);
+        assertTrue(true);
+
+    }
 }
 
