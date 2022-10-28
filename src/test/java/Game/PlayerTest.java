210414ef7b0a9d950e45443bd533ed22920c8400
Navaty Khara <navatykhara@gmail.com>
2022-10-28 15:06:52 -0400

    U-TEST for setter function for score in Player

--- src/test/java/Game/PlayerTest.java
+++ src/test/java/Game/PlayerTest.java
@@ -11,4 +11,11 @@ class PlayerTest {
         Player p = new Player();
         assertEquals(p.getScore(), 0);
     }
+
+    @Test
+    void setScore() {
+        Player p = new Player();
+        p.setScore(1);
+        assertEquals(p.getScore(), 1);
+    }
 }
