9258f3ef6a907d807ddb983103124d0997c08e48
Navaty Khara <navatykhara@gmail.com>
2022-10-27 21:20:31 -0400

    U-TEST for testing that initialize() from the Fortunes class properly initializes the correct distribution of Fortune cards.

---
+++ src/test/java/FortuneCards/FortunesTest.java
@@ -0,0 +1,65 @@
+package FortuneCards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FortunesTest {
+
+    @Test
+    void initialize() {
+        FortuneCards[] temp = new FortuneCards[]{
+                (FortuneCards) new TreasureChest(),
+                (FortuneCards) new TreasureChest(),
+                (FortuneCards) new TreasureChest(),
+                (FortuneCards) new TreasureChest(),
+                (FortuneCards) new Sorceress(),
+                (FortuneCards) new Sorceress(),
+                (FortuneCards) new Sorceress(),
+                (FortuneCards) new Sorceress(),
+                (FortuneCards) new Captain(),
+                (FortuneCards) new Captain(),
+                (FortuneCards) new Captain(),
+                (FortuneCards) new Captain(),
+                (FortuneCards) new MonkeyBusiness(),
+                (FortuneCards) new MonkeyBusiness(),
+                (FortuneCards) new MonkeyBusiness(),
+                (FortuneCards) new MonkeyBusiness(),
+                (FortuneCards) new Diamond(),
+                (FortuneCards) new Diamond(),
+                (FortuneCards) new Diamond(),
+                (FortuneCards) new Diamond(),
+                (FortuneCards) new Coin(),
+                (FortuneCards) new Coin(),
+                (FortuneCards) new Coin(),
+                (FortuneCards) new Coin(),
+                (FortuneCards) new Skulls(2),
+                (FortuneCards) new Skulls(2),
+                (FortuneCards) new Skulls(1),
+                (FortuneCards) new Skulls(1),
+                (FortuneCards) new Skulls(1),
+                (FortuneCards) new SeaBattle(2),
+                (FortuneCards) new SeaBattle(2),
+                (FortuneCards) new SeaBattle(3),
+                (FortuneCards) new SeaBattle(3),
+                (FortuneCards) new SeaBattle(4),
+                (FortuneCards) new SeaBattle(4)
+        };
+
+        Fortunes fortune = new Fortunes();
+        int index = 0;
+        for(FortuneCards f : fortune.initialize()){
+            if(!f.toString().equals(temp[index].toString()))
+                assertTrue(false);
+            if(f.getClass().equals(Skulls.class))
+                assertEquals(((Skulls) f).getSkulls(), ((Skulls) temp[index]).getSkulls());
+            if(f.getClass().equals(SeaBattle.class))
+                assertEquals(((SeaBattle) f).getSwords(), ((SeaBattle) temp[index]).getSwords());
+            index++;
+        }
+        assertTrue(true);
+    }
+
+
+}
