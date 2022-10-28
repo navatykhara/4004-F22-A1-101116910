230ca9c1d3607f765439cae6d428d916abeb6e8d
Navaty Khara <navatykhara@gmail.com>
2022-10-28 15:55:35 -0400

    U-TEST for testing if a Player is alive

--- src/test/java/Game/PlayerTest.java
+++ src/test/java/Game/PlayerTest.java
@@ -1,5 +1,7 @@
 package Game;
 
+import Dice.Dice;
+import Dice.DiceState;
 import Dice.Hand;
 import FortuneCards.Coin;
 import org.junit.jupiter.api.Test;
@@ -48,4 +50,43 @@ class PlayerTest {
         assertTrue(p.getHand().getClass().equals(Hand.class));
     }
 
+
+    @Test
+    void isAliveBaseCase(){
+        Player p = new Player(1);
+
+        Hand hand = new Hand();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SKULL);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.COIN);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.COIN);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.COIN);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        hand.cleanUp();
+
+        p.getHand().setHand(hand.getHand());
+        assertTrue(p.isAlive());
+    }
 }
