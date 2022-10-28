7fe3ae968d049f2ceb46d59845ab39764a420f2d
Navaty Khara <navatykhara@gmail.com>
2022-10-28 16:27:44 -0400

    U-TEST for testing Sorceress reroll with one skull

--- src/test/java/Dice/HandTest.java
+++ src/test/java/Dice/HandTest.java
@@ -2,6 +2,7 @@ package Dice;
 
 import FortuneCards.Coin;
 import FortuneCards.Skulls;
+import FortuneCards.Sorceress;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -337,6 +338,50 @@ class HandTest {
         hand.cleanUp();
 
         assertEquals(hand.getNumSkulls(new Coin()), 1);
+
+    }
+
+    @Test
+    void rollSkullsWithoutSorceress(){
+        Hand hand = new Hand();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SKULL);
+        d2.setDice(DiceState.COIN);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.COIN);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.COIN);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.COIN);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+
+        Dice d = new Dice();
+        d.setDice(DiceState.PARROT);
+        hand.rollSkull(new Sorceress(), d);
+        //Check to see if skulls are removed properly
+        assertEquals(hand.getNumSkulls(null), 0);
+        //Check to see if dice is rolled properly
+        assertEquals(hand.getHand()[hand.getHand().length-1], d);
 
     }
 }
