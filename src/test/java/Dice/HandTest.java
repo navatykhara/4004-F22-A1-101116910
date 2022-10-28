0a940ff119e4a889814e52c81e6aebe9e9081b32
Navaty Khara <navatykhara@gmail.com>
2022-10-27 22:28:30 -0400

    U-TEST for testing cleanup but with zero skulls

--- src/test/java/Dice/HandTest.java
+++ src/test/java/Dice/HandTest.java
@@ -76,5 +76,44 @@ class HandTest {
         assertEquals(countNulls, hand.getHand().length);
     }
 
+    @Test
+    void cleanUpNoSkulls(){
+        Hand hand = new Hand();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.COIN);
+        d2.setDice(DiceState.COIN);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.COIN);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.COIN);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.COIN);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        hand.cleanUp();
+        int countNulls = 0;
+        for(Dice d : hand.getHand())
+            if(d == null)
+                countNulls++;
+
+        assertEquals(countNulls, 0);
+    }
 }
 
