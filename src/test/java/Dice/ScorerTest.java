3464afac7939f069247676c1cab44348cdb2c7a8
Navaty Khara <navatykhara@gmail.com>
2022-10-28 00:39:37 -0400

    U-TEST for differentiating between value dice [DIAMOND, COIN] and non-value dice for scoring functions

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -85,7 +85,40 @@ public class ScorerTest {
         scorer.count(hand);
         assertEquals(scorer.score(), 100);
     }
+    @Test
+    void testCoin() {
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
 
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
 
+        d1.setDice(DiceState.COIN);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.SKULL);
+        d4.setDice(DiceState.MONKEY);
+        d5.setDice(DiceState.MONKEY);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        scorer.count(hand);
+        assertEquals(scorer.score(), 100);
+    }
 
 }
