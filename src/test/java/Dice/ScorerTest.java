f4cf6998f0fcdd9236246045b5a5af9ca0888399
Navaty Khara <navatykhara@gmail.com>
2022-10-28 14:34:32 -0400

    U-TEST for testing Sea Battle fortune for winning bonus in scoring

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -1,9 +1,6 @@
 package Dice;
 
-import FortuneCards.Captain;
-import FortuneCards.Coin;
-import FortuneCards.Diamond;
-import FortuneCards.MonkeyBusiness;
+import FortuneCards.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -577,7 +574,6 @@ public class ScorerTest {
         scorer.count(hand);
         assertEquals(scorer.score(), 400);
     }
-
     @Test
     void testMonkeyBusinessScoring(){
         Hand hand = new Hand();
@@ -613,6 +609,42 @@ public class ScorerTest {
         scorer.setFortune(new MonkeyBusiness());
         scorer.count(hand);
         assertEquals(scorer.score(), 200);
+    }
+    @Test
+    void testSeaBattleGetBonusScoring(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SWORD);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.SKULL);
+        d4.setDice(DiceState.SWORD);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.MONKEY);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        scorer.setFortune(new SeaBattle(2));
+        scorer.count(hand);
+        assertEquals(scorer.score(), 400);
     }
 
 }
