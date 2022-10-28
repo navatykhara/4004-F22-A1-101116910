fa0b7b2cf6f4e1112b788f3f6174d18e1a4f5b8d
Navaty Khara <navatykhara@gmail.com>
2022-10-27 20:48:47 -0400

    CODE for implementation of useRoll method of SORCERESS and adjacent variables

---
+++ src/main/java/FortuneCards/Sorceress.java
@@ -0,0 +1,15 @@
+package FortuneCards;
+
+public class Sorceress implements FortuneCards{
+
+    int roll = 1;
+
+    public void useRoll(){
+        roll = 0;
+    }
+
+    public int getRoll(){
+        return roll;
+    }
+
+}
