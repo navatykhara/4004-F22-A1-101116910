281483ad4b1e13caec8d6fdbc31a3e18bd771180
Navaty Khara <navatykhara@gmail.com>
2022-10-27 20:25:47 -0400

    U-TEST for testing getter method of SKULLS

---
+++ src/test/java/FortuneCards/SkullsTest.java
@@ -0,0 +1,16 @@
+package FortuneCards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SkullsTest {
+
+    @Test
+    void getSkulls() {
+        int skulls = 1;
+        Skulls s = new Skulls(skulls);
+        assertEquals(s.getSkulls(), skulls);
+    }
+    
+}
