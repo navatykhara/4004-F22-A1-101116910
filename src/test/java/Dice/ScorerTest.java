2abbcfe64b4c1abd32e0e5f7548303d1577b3bb2
Navaty Khara <navatykhara@gmail.com>
2022-10-28 11:57:23 -0400

    U-TEST for testing bonus scoring rule in score function in scorer

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -360,4 +360,39 @@ public class ScorerTest {
         scorer.count(hand);
         assertEquals(scorer.score() - 500, 4000);
     }
+    @Test
+    void testFullChestBonus(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.PARROT);
+        d2.setDice(DiceState.PARROT);
+        d3.setDice(DiceState.PARROT);
+        d4.setDice(DiceState.PARROT);
+        d5.setDice(DiceState.PARROT);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.PARROT);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+        scorer.count(hand);
+
+        assertEquals(scorer.score() - 4000, 500);
+    }
 }
