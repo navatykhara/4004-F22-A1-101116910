f0bec5e029c129b6412dcff00411dbd710772081
Navaty Khara <navatykhara@gmail.com>
2022-10-28 00:59:28 -0400

    U-TEST for testing quadra scoring rule in score function in scorer

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -190,5 +190,38 @@ public class ScorerTest {
 
         assertEquals(scorer.score(), 100);
     }
+    @Test
+    void testQuadraScoring(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SKULL);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.MONKEY);
+        d4.setDice(DiceState.PARROT);
+        d5.setDice(DiceState.PARROT);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+        scorer.count(hand);
+        assertEquals(scorer.score(), 200);
+    }
 
 }
