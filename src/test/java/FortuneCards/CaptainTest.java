1931a9d1ce5c4b7c3e1e55673ef8a843484a7b94
Navaty Khara <navatykhara@gmail.com>
2022-10-27 20:03:21 -0400

    U-TEST for testing toString method of CAPTAIN

---
+++ src/test/java/FortuneCards/CaptainTest.java
@@ -0,0 +1,15 @@
+package FortuneCards;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CaptainTest {
+
+    @Test
+    void testToString() {
+        Captain c = new Captain();
+        assertEquals(c.toString(), "CAPTAIN");
+    }
+    
+}
