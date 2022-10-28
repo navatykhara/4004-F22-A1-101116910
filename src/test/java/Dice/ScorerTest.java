800d869d3af6dcaf368f8c792836fef301889446
Navaty Khara <navatykhara@gmail.com>
2022-10-28 12:28:54 -0400

    U-TEST for testing scoring rules for Captain fortune card

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -1,5 +1,6 @@
 package Dice;
 
+import FortuneCards.Captain;
 import FortuneCards.Coin;
 import org.junit.jupiter.api.Test;
 
@@ -394,5 +395,42 @@ public class ScorerTest {
         scorer.count(hand);
 
         assertEquals(scorer.score() - 4000, 500);
+    }
+
+    @Test
+    void testCaptainScoring(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.DIAMOND);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.SKULL);
+        d4.setDice(DiceState.MONKEY);
+        d5.setDice(DiceState.MONKEY);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        scorer.setFortune(new Captain());
+        scorer.count(hand);
+        assertEquals(scorer.score(), 200);
     }
 }
