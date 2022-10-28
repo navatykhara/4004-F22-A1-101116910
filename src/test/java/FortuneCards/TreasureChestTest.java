eeb24b641381ed7ddd8cbece9cc836b95ff9478c
Navaty Khara <navatykhara@gmail.com>
2022-10-27 21:08:24 -0400

    U-TEST for testing getHand method of TREASURECHEST. The test checks to see that the get method returns a hand.

---
+++ src/test/java/FortuneCards/TreasureChestTest.java
@@ -0,0 +1,16 @@
+package FortuneCards;
+
+import Dice.Hand;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreasureChestTest {
+
+    @Test
+    void getHand() {
+        TreasureChest tc = new TreasureChest();
+        assertEquals(tc.getHand().getClass(), Hand.class);
+    }
+
+}
