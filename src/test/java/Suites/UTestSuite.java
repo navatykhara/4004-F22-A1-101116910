b732288198a83888cee31ecacbf9f106b6e3cf0d
Navaty Khara <navatykhara@gmail.com>
2022-10-28 16:09:04 -0400

    REFACTOR for adding Game package to Suite. Reorganization.

--- src/test/java/Suites/UTestSuite.java
+++ src/test/java/Suites/UTestSuite.java
@@ -3,7 +3,7 @@ package Suites;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-@SelectPackages({"Dice", "FortuneCards"})
+@SelectPackages({"Dice", "FortuneCards", "Game"})
 @Suite
 public class UTestSuite {
 }
