9bd75138c332cb748a8cd653ca6a371318fa4570
Navaty Khara <navatykhara@gmail.com>
2022-10-28 14:51:39 -0400

    U-TEST for testing getter function for non-SKULL fortune cards

--- src/test/java/Dice/HandTest.java
+++ src/test/java/Dice/HandTest.java
@@ -1,5 +1,6 @@
 package Dice;
 
+import FortuneCards.Coin;
 import FortuneCards.Skulls;
 import org.junit.jupiter.api.Test;
 
@@ -265,7 +266,7 @@ class HandTest {
     }
     @Test
     void getNumSkulls(){
-        
+
         Hand hand = new Hand();
 
         Dice d1 = new Dice();
@@ -298,6 +299,44 @@ class HandTest {
         hand.cleanUp();
 
         assertEquals(hand.getNumSkulls(new Skulls(1)), 2);
+
+    }
+
+    @Test
+    void getNumSkullsForNonSkullFortunes(){
+
+        Hand hand = new Hand();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.COIN);
+        d2.setDice(DiceState.COIN);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.COIN);
+        d5.setDice(DiceState.COIN);
+        d6.setDice(DiceState.COIN);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.SKULL);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        hand.cleanUp();
+
+        assertEquals(hand.getNumSkulls(new Coin()), 1);
 
     }
 }
