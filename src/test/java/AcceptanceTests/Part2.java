5e3914154426c8b07e0bb5a7b7d52f1bbc3bdfd5
Navaty Khara <navatykhara@gmail.com>
2022-10-29 11:12:21 -0400

    A-TEST for row78

--- src/test/java/AcceptanceTests/Part2.java
+++ src/test/java/AcceptanceTests/Part2.java
@@ -80,4 +80,72 @@ public class Part2 {
         scorer.count(hand);
         assertEquals(scorer.score(), 500);
     }
+    @Test
+    void row78(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SKULL);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.SKULL);
+        d4.setDice(DiceState.PARROT);
+        d5.setDice(DiceState.PARROT);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.SWORD);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        Sorceress sc = new Sorceress();
+        Dice skullRoll = new Dice();
+        skullRoll.setDice(DiceState.PARROT);
+        hand.rollSkull(sc, skullRoll);
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        Dice rolled_d1 = new Dice();
+        Dice rolled_d2 = new Dice();
+
+        rolled_d1.setDice(DiceState.PARROT);
+        rolled_d2.setDice(DiceState.PARROT);
+
+        hand.setHand(new Dice[]{ hand.getHand()[0],
+                hand.getHand()[1],
+                hand.getHand()[2],
+                hand.getHand()[3],
+                rolled_d1,
+                rolled_d2,
+                hand.getHand()[6],
+                hand.getHand()[7]});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(sc);
+        scorer.count(hand);
+        assertEquals(scorer.score(), 1000);
+    }
 }
