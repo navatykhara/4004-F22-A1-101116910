44e16efbcf2582a9028b629354ac3b75fda85284
Navaty Khara <navatykhara@gmail.com>
2022-10-29 13:55:55 -0400

    A-TEST for row98

--- src/test/java/AcceptanceTests/Part2.java
+++ src/test/java/AcceptanceTests/Part2.java
@@ -2,10 +2,7 @@ package AcceptanceTests;
 
 import Dice.*;
 
-import FortuneCards.Coin;
-import FortuneCards.MonkeyBusiness;
-import FortuneCards.Sorceress;
-import FortuneCards.TreasureChest;
+import FortuneCards.*;
 
 import Game.*;
 
@@ -593,5 +590,45 @@ public class Part2 {
         scorer.setFortune(new Coin());
         scorer.count(hand);
         assertEquals(scorer.score(), 400);
+    }
+    @Test
+    void row98(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.MONKEY);
+        d2.setDice(DiceState.MONKEY);
+        d3.setDice(DiceState.MONKEY);
+        d4.setDice(DiceState.SWORD);
+        d5.setDice(DiceState.SWORD);
+        d6.setDice(DiceState.SWORD);
+        d7.setDice(DiceState.COIN);
+        d8.setDice(DiceState.COIN);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(new Captain());
+        scorer.count(hand);
+        assertEquals(scorer.score(), 1800);
     }
 }
