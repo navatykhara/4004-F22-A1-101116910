60808f87c3b1ddb1c43b837fb02ecd6d25097ed1
Navaty Khara <navatykhara@gmail.com>
2022-10-29 01:16:01 -0400

    U-TEST for testing UI for removing dice from chest

--- src/test/java/Game/GameTest.java
+++ src/test/java/Game/GameTest.java
@@ -384,7 +384,6 @@ public class GameTest {
 
         assertEquals(game.addToChestForPlayer(p1,new int[]{0,1}), "Player 1 does not have the TREASURE CHEST fortune card.");
     }
-
     @Test
     void addToChestForPlayerInvalidInput(){
         Player p1 = new Player(1);
@@ -423,5 +422,46 @@ public class GameTest {
         Game game = new Game(new Player[]{p1});
 
         assertEquals(game.addToChestForPlayer(p1,new int[]{-1,8}), "Player 1 has not provided valid input.");
+    }
+    @Test
+    void removeFromChestForPlayer(){
+        Player p1 = new Player(1);
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.COIN);
+        d2.setDice(DiceState.MONKEY);
+        d3.setDice(DiceState.PARROT);
+        d4.setDice(DiceState.PARROT);
+        d5.setDice(DiceState.PARROT);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.PARROT);
+
+        Dice[] d = new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8};
+
+        p1.getHand().setHand(d);
+        p1.getHand().cleanUp();
+        p1.setFortune(new TreasureChest());
+
+        Game game = new Game(new Player[]{p1});
+        game.addToChestForPlayer(p1,new int[]{0,1});
+
+
+        assertEquals(game.removeFromChestForPlayer(p1, new int[]{0,1}), "Player 1 removes [C][M] from the TREASURE CHEST resulting in ");
     }
 }
