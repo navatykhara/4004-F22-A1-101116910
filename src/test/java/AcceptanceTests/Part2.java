b79bd3504193594168f2f1f82bd5634c9601e79e
Navaty Khara <navatykhara@gmail.com>
2022-10-29 13:54:35 -0400

    A-TEST for row97

--- src/test/java/AcceptanceTests/Part2.java
+++ src/test/java/AcceptanceTests/Part2.java
@@ -554,5 +554,44 @@ public class Part2 {
 
         System.out.println("Player " + p1.getId() + " has died.");
     }
+    @Test
+    void row97(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
 
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.MONKEY);
+        d2.setDice(DiceState.MONKEY);
+        d3.setDice(DiceState.MONKEY);
+        d4.setDice(DiceState.SWORD);
+        d5.setDice(DiceState.SWORD);
+        d6.setDice(DiceState.SWORD);
+        d7.setDice(DiceState.DIAMOND);
+        d8.setDice(DiceState.PARROT);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(new Coin());
+        scorer.count(hand);
+        assertEquals(scorer.score(), 400);
+    }
 }
