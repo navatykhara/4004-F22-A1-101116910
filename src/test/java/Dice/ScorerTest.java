7d0b4fea99790f057f5526a0629cbed14e6cdc98
Navaty Khara <navatykhara@gmail.com>
2022-10-29 02:10:23 -0400

    U-TEST for testing dead player scoring

--- src/test/java/Dice/ScorerTest.java
+++ src/test/java/Dice/ScorerTest.java
@@ -684,4 +684,43 @@ public class ScorerTest {
         assertEquals(scorer.score(), -300);
     }
 
+    @Test
+    void deadScoring(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.DIAMOND);
+        d2.setDice(DiceState.SKULL);
+        d3.setDice(DiceState.SKULL);
+        d4.setDice(DiceState.MONKEY);
+        d5.setDice(DiceState.MONKEY);
+        d6.setDice(DiceState.PARROT);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        scorer.count(hand);
+        scorer.setAlive(false);
+
+        assertEquals(scorer.score(), 0);
+
+    }
+
 }
