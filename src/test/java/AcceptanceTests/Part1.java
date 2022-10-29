b2ccce7336c6c7dd09e29f2d785013cf7a05bdc7
Navaty Khara <navatykhara@gmail.com>
2022-10-29 10:38:41 -0400

    A-TEST for row62

--- src/test/java/AcceptanceTests/Part1.java
+++ src/test/java/AcceptanceTests/Part1.java
@@ -805,4 +805,44 @@ public class Part1 {
         scorer.count(hand);
         assertEquals(scorer.score(), 600);
     }
+    @Test
+    void row62(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.MONKEY);
+        d2.setDice(DiceState.MONKEY);
+        d3.setDice(DiceState.MONKEY);
+        d4.setDice(DiceState.MONKEY);
+        d5.setDice(DiceState.MONKEY);
+        d6.setDice(DiceState.MONKEY);
+        d7.setDice(DiceState.SKULL);
+        d8.setDice(DiceState.SKULL);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(new Coin());
+        scorer.count(hand);
+        assertEquals(scorer.score(), 1100);
+    }
 }
