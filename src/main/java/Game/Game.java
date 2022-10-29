822072eb19f7b5923feec51072ac76e62ba10d6c
Navaty Khara <navatykhara@gmail.com>
2022-10-28 22:16:55 -0400

    CODE for implementing check winners function for checking who is the winner. The highest score is the winner. In the case of a tie, the first person to achieve said score is the winner

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -1,6 +1,12 @@
 package Game;
 
 import Dice.Scorer;
+import Dice.SortHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Game {
 
@@ -39,6 +45,25 @@ public class Game {
             }
         }
         return false;
+    }
+
+    public String checkWinner(){
+        String msg = "";
+        ArrayList<Player> sort = new ArrayList<>();
+
+        for(Player p : players){
+            if(p.getTotal() >= 3000) {
+                sort.add(p);
+            }
+        }
+
+        Collections.sort(sort, (p1, p2) -> {
+            return p2.getTotal() - p1.getTotal();
+        });
+
+        msg += "Player " + sort.get(0).getId() + " won with " + sort.get(0).getTotal() + " points.";
+
+        return msg;
     }
 
 }
