0c5054ebad764d94d3a169157711312204bd8a64
Navaty Khara <navatykhara@gmail.com>
2022-10-29 10:59:04 -0400

    A-TEST for row70

--- src/test/java/AcceptanceTests/Part1.java
+++ src/test/java/AcceptanceTests/Part1.java
@@ -1182,4 +1182,65 @@ public class Part1 {
         scorer.count(hand);
         assertEquals(scorer.score(), 500);
     }
+    @Test
+    void row70(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.SKULL);
+        d2.setDice(DiceState.COIN);
+        d3.setDice(DiceState.COIN);
+        d4.setDice(DiceState.MONKEY);
+        d5.setDice(DiceState.PARROT);
+        d6.setDice(DiceState.SWORD);
+        d7.setDice(DiceState.SWORD);
+        d8.setDice(DiceState.SWORD);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        Dice rolled_d1 = new Dice();
+        Dice rolled_d2 = new Dice();
+        Dice rolled_d3 = new Dice();
+
+        rolled_d1.setDice(DiceState.COIN);
+        rolled_d2.setDice(DiceState.MONKEY);
+        rolled_d3.setDice(DiceState.PARROT);
+
+        hand.setHand(new Dice[]{ hand.getHand()[0],
+                hand.getHand()[1],
+                hand.getHand()[2],
+                hand.getHand()[3],
+                rolled_d1,
+                rolled_d2,
+                rolled_d3,
+                hand.getHand()[7]});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(new Coin());
+        scorer.count(hand);
+        assertEquals(scorer.score(), 600);
+    }
 }
