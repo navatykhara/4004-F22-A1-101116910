06bf8e934c6bd9cb2041d8987aecda0e14f95de5
Navaty Khara <navatykhara@gmail.com>
2022-10-29 14:58:46 -0400

    A-TEST for row124

--- src/test/java/AcceptanceTests/Part2.java
+++ src/test/java/AcceptanceTests/Part2.java
@@ -1204,4 +1204,92 @@ public class Part2 {
         scorer.count(hand);
         assertEquals(scorer.score(), 1300);
     }
+    @Test
+    void row124(){
+        Hand hand = new Hand();
+        Scorer scorer = new Scorer();
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        d1.setDice(DiceState.MONKEY);
+        d2.setDice(DiceState.MONKEY);
+        d3.setDice(DiceState.MONKEY);
+        d4.setDice(DiceState.SWORD);
+        d5.setDice(DiceState.SKULL);
+        d6.setDice(DiceState.DIAMOND);
+        d7.setDice(DiceState.PARROT);
+        d8.setDice(DiceState.PARROT);
+
+        hand.setHand(new Dice[]{ d1,
+                d2,
+                d3,
+                d4,
+                d5,
+                d6,
+                d7,
+                d8});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        //REROLL 2 PARROTS
+
+        Dice rolled_d1 = new Dice();
+        Dice rolled_d2 = new Dice();
+
+        rolled_d1.setDice(DiceState.SWORD);
+        rolled_d2.setDice(DiceState.SWORD);
+
+
+        hand.setHand(new Dice[]{
+                hand.getHand()[0],
+                hand.getHand()[1],
+                hand.getHand()[2],
+                hand.getHand()[3],
+                rolled_d1,
+                rolled_d2,
+                hand.getHand()[6],
+                hand.getHand()[7]});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        //REROLL 3 MONKEYS
+
+        Dice rolled_k1 = new Dice();
+        Dice rolled_k2 = new Dice();
+        Dice rolled_k3 = new Dice();
+
+        rolled_k1.setDice(DiceState.SWORD);
+        rolled_k2.setDice(DiceState.PARROT);
+        rolled_k3.setDice(DiceState.PARROT);
+
+
+        hand.setHand(new Dice[]{
+                hand.getHand()[0],
+                rolled_k1,
+                rolled_k2,
+                rolled_k3,
+                hand.getHand()[4],
+                hand.getHand()[5],
+                hand.getHand()[6],
+                hand.getHand()[7]});
+
+        Arrays.sort(hand.getHand(), new SortHelper());
+        hand.cleanUp();
+        System.out.println(hand.toString());
+
+        scorer.setFortune(new SeaBattle(4));
+        scorer.count(hand);
+        assertEquals(scorer.score(), 1300);
+    }
 }
