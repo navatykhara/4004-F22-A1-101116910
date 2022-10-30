8226d6a3b1407000f8fa00b9b2561904ede528c8
Navaty Khara <navatykhara@gmail.com>
2022-10-30 16:47:43 -0400

    NETWORKING for networking the game together

---
+++ src/main/java/Network/Client.java
@@ -0,0 +1,199 @@
+package Network;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class Client {
+
+    private BufferedReader reader;
+    private String name = "";
+    private ClientSideConnection csc;
+
+    private int playerID;
+
+
+    public Client() {
+
+
+        reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+
+    public static void main(String[] args) {
+
+        Client app = new Client();
+
+        try {
+            app.name = args[0];
+        } catch (Exception e) {
+        }
+
+        app.connectToServer();
+        app.startReceivingInfo();
+
+    }
+
+    public void startReceivingInfo() {
+
+        Thread T = new Thread(new Runnable() {
+
+            public void run() {
+
+                //Game is starting soon
+                System.out.println(csc.receiveFromServer());
+                boolean gameEnded = false;
+
+                while(!gameEnded) {
+
+                    String msg = csc.receiveFromServer();
+                    boolean myTurn = false;
+
+                    while(!myTurn) {
+                        if(msg.equals("It is Player " + playerID + "'s turn.")){
+                            myTurn = true;
+                            break;
+                        }else{
+                            myTurn = false;
+                        }
+
+                        //Scoreboard
+                        System.out.println(msg);
+
+                        //Check win
+                        if(msg.contains("won with")){
+                            myTurn = false;
+                            gameEnded = true;
+                            break;
+                        }
+                        msg = csc.receiveFromServer();
+
+                    }
+
+                    if(myTurn) {
+                        //Player turn
+                        System.out.println(msg);
+
+                        msg = csc.receiveFromServer();
+
+                        //Fortune
+                        System.out.println(msg);
+
+                        while(true) {
+                            //Player Hand
+                            System.out.println(csc.receiveFromServer());
+
+                            msg = csc.receiveFromServer();
+
+                            if(msg.equals("Player " + playerID + " has died.")){
+                                System.out.println(msg);
+                                break;
+                            }
+
+                            //Player Score
+                            System.out.println(msg);
+
+                            //Player choices
+                            System.out.println(csc.receiveFromServer());
+
+                            try {
+                                msg = reader.readLine();
+                                csc.sendToServer(msg);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+
+                            msg = csc.receiveFromServer();
+
+
+
+                            if(msg.equals("Choose the dice index you wish to roll (0-7) [no spaces]") ||
+                                    msg.equals("Choose the dice index you wish to add to treasure chest (0-7) [no spaces]")
+                                    || msg.equals("Choose the dice index you wish to remove from treasure chest (0-7) [no spaces]")){
+
+                                System.out.println(msg);
+                                try {
+                                    msg = reader.readLine();
+                                    csc.sendToServer(msg);
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                //Game message
+                                System.out.println(csc.receiveFromServer());
+                            }else if(!msg.equals("Player " + playerID + "'s turn ended.")){ //Reroll skulls case
+                                //Rerolled skull message
+                                System.out.println(msg);
+                            }else{
+                                break;
+                            }
+
+                        }
+                        myTurn = false;
+                    }
+
+                }
+            }
+
+
+        });
+
+        T.start();
+    }
+
+    private void connectToServer() {
+        csc = new ClientSideConnection();
+    }
+
+    private class ClientSideConnection {
+
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+
+        public ClientSideConnection() {
+
+            try {
+
+                socket = new Socket("localhost", 90);
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+                playerID = dataIn.readInt();
+
+            } catch (IOException ex) {
+                System.out.println("IO Exception from CSC construction");
+            }
+
+        }
+
+        public void sendToServer(String res) {
+
+            try {
+                dataOut.writeUTF(res);
+                dataOut.flush();
+            } catch (IOException e) {
+                System.out.println("IOException from sendToServer() CSC");
+            }
+        }
+
+        public String receiveFromServer() {
+
+            String res = "";
+
+            try {
+                res = dataIn.readUTF();
+            } catch (IOException e) {
+                System.out.println("IOException from recieveFromServer() CSC");
+            }
+
+            return res;
+        }
+    }
+
+}
---
+++ src/main/java/Network/Server.java
@@ -0,0 +1,260 @@
+package Network;
+
+import FortuneCards.Skulls;
+import FortuneCards.TreasureChest;
+import Game.Game;
+import Game.Player;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Server {
+
+    private ServerSocket ss;
+    private int numPlayers;
+    private ServerSideConnection player1;
+    private ServerSideConnection player2;
+    private ServerSideConnection player3;
+
+    private ServerSideConnection[] players;
+    private List<String> playersList;
+    private Player[] playerObjectList;
+
+    private Game game;
+
+    public List<String> getPlayers() {
+        return playersList;
+    }
+
+    public Server() {
+
+        playersList = new ArrayList<String>();
+        numPlayers = 0;
+
+        Player p1 = new Player(1);
+        Player p2 = new Player(2);
+        Player p3 = new Player(3);
+
+        playerObjectList = new Player[]{p1, p2, p3};
+
+        game = new Game(playerObjectList);
+
+        System.out.println("Piraten Kapern Game is starting");
+
+        try {
+            ss = new ServerSocket(90);
+        } catch (IOException e) {
+            System.out.println("IOException from Server constructor");
+        }
+    }
+
+    public void acceptConnections() {
+
+        try {
+            System.out.println("\nWaiting for players to join ...");
+
+            while (numPlayers < 3) {
+
+                Socket s = ss.accept();
+                numPlayers++;
+                ServerSideConnection SSC = new ServerSideConnection(s, numPlayers);
+
+                if (numPlayers == 1) {
+                    player1 = SSC;
+                } else if (numPlayers == 2) {
+                    player2 = SSC;
+                } else {
+                    player3 = SSC;
+                }
+                players = new ServerSideConnection[]{player1, player2, player3};
+
+                Thread t = new Thread(SSC);
+                t.start();
+
+            }
+            ss.close();
+
+        } catch (IOException ex) {
+            System.out.println("IOException from accepting connections()");
+        }
+    }
+
+    public static void main(String[] args) {
+        Server s = new Server();
+        s.acceptConnections();
+    }
+
+    private class ServerSideConnection implements Runnable {
+        private Socket socket;
+        private DataInputStream dataIn;
+        private DataOutputStream dataOut;
+        private int playerID;
+
+        private int winningRound;
+
+        ServerSideConnection(Socket s, int id) {
+            winningRound = 2;
+            socket = s;
+            playerID = id;
+            try {
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("IOException from run() SSC construction.");
+            }
+        }
+
+        public void run() {
+
+            try {
+                dataOut.writeInt(playerID);
+                dataOut.flush();
+
+                System.out.println("Player " + playerID + " has entered the game lobby!");
+
+                playersList.add(Integer.toString(playerID));
+
+                if (numPlayers == 3) {
+
+                    String msg = "Game is starting soon.";
+
+                    player1.sendToClient(msg);
+                    player2.sendToClient(msg);
+                    player3.sendToClient(msg);
+
+                    game.getFortuneDeck().shuffle();
+
+                    do {
+
+                        for(int i = 0; i < playerObjectList.length; i++) {
+
+                            for(int j = 0; j < 3; j++) {
+                                players[j].sendToClient(game.getScoreBoard());
+                            }
+
+                            for(int j = 0; j < 3; j++) {
+                                players[j].sendToClient("It is Player " + playerObjectList[i].getId() + "'s turn.");
+                                if(j+1 == playerObjectList[i].getId())
+                                    players[j].sendToClient(game.drawFortuneForPlayer(playerObjectList[i]));
+
+                            }
+
+                            game.getScorer().setFortune(playerObjectList[i].getFortune());
+                            playerObjectList[0].setTotal(4000);
+                            playerObjectList[i].getHand().initialize();
+                            game.getScorer().setAlive(playerObjectList[i].isAlive());
+
+                            //Main Game Loop
+                            while(true){
+
+                                if(playerObjectList[i].getFortune().getClass().equals(TreasureChest.class))
+                                    players[i].sendToClient("Player " + playerObjectList[i].getId() + "'s hand = " + playerObjectList[i].getHand().toString() + " | " + playerObjectList[i].getHand().getChest(playerObjectList[i].getFortune()).toString(playerObjectList[i].getFortune()));
+                                else
+                                    players[i].sendToClient("Player " + playerObjectList[i].getId() + "'s hand = " + playerObjectList[i].getHand().toString());
+
+                                if(!playerObjectList[i].isAlive()){
+                                    players[i].sendToClient("Player " + playerObjectList[i].getId() + " has died.");
+                                    game.getScorer().setAlive(false);
+                                    game.getScoreForPlayer(playerObjectList[i]);
+                                    playerObjectList[i].setTotal((playerObjectList[i].getScore() + playerObjectList[i].getTotal()) < 0 ? 0 : (playerObjectList[i].getScore() + playerObjectList[i].getTotal()));
+                                    break;
+                                }
+
+                                players[i].sendToClient(game.getScoreForPlayer(playerObjectList[i]));
+
+                                players[i].sendToClient(game.offerChoicesForPlayer());
+
+                                msg = players[i].receiveFromClient();
+                                int choice = Integer.parseInt(msg);
+                                boolean endTurn = false;
+
+                                switch(choice){
+                                    case 1:
+                                        players[i].sendToClient("Choose the dice index you wish to roll (0-7) [no spaces]");
+                                        String input = players[i].receiveFromClient();
+                                        int[] temp = new int [input.length()];
+                                        for(int k = 0; k < temp.length; k++)
+                                            temp[k] = Integer.parseInt(input, k, k+1,10);
+                                        players[i].sendToClient(game.rollDiceForPlayer(playerObjectList[i], temp));
+                                        break;
+                                    case 2:
+                                        players[i].sendToClient(game.rollSkullForPlayer(playerObjectList[i]));
+                                        break;
+                                    case 3:
+                                        players[i].sendToClient("Choose the dice index you wish to add to treasure chest (0-7) [no spaces]");
+                                        input = players[i].receiveFromClient();
+                                        temp = new int [input.length()];
+                                        for(int k = 0; k < temp.length; k++)
+                                            temp[k] = Integer.parseInt(input, k, k+1,10);
+                                        players[i].sendToClient(game.addToChestForPlayer(playerObjectList[i], temp));
+                                        break;
+                                    case 4:
+                                        players[i].sendToClient("Choose the dice index you wish to remove from treasure chest (0-7) [no spaces]");
+                                        input = players[i].receiveFromClient();
+                                        temp = new int [input.length()];
+                                        for(int k = 0; k < temp.length; k++)
+                                            temp[k] = Integer.parseInt(input, k, k+1,10);
+                                        players[i].sendToClient(game.removeFromChestForPlayer(playerObjectList[i], temp));
+                                        break;
+                                    case 5:
+                                        players[i].sendToClient("Player " + playerObjectList[i].getId() + "'s turn ended.");
+                                        playerObjectList[i].setTotal((playerObjectList[i].getScore() + playerObjectList[i].getTotal()) < 0 ? 0 : (playerObjectList[i].getScore() + playerObjectList[i].getTotal()));
+                                        endTurn = true;
+                                        break;
+                                    default:
+                                }
+
+                                if(endTurn)
+                                    break;
+
+                            }
+
+                        }
+
+                        if(game.checkWinCondition()){
+                            winningRound--;
+                        }else{
+                            winningRound=2;
+                        }
+
+                    }while(!game.checkWinCondition() || winningRound > 0);
+
+                    for(int i = 0; i < 3; i++) {
+                        players[i].sendToClient(game.checkWinner());
+                    }
+
+                }
+
+
+            } catch (IOException e) {
+                System.out.println("IOException from run() SSC");
+            }
+        }
+        public void sendToClient(String chart) {
+
+            try {
+                dataOut.writeUTF(chart);
+                dataOut.flush();
+            } catch (IOException e) {
+                System.out.println("IOException from sendcharts() SSC");
+            }
+        }
+
+        public String receiveFromClient() {
+
+            String res = "";
+            try {
+                res = dataIn.readUTF();
+            } catch (IOException e) {
+                System.out.println("IOException from recieveFromServer() CSC");
+            }
+
+            return res;
+        }
+
+
+    }
+}
