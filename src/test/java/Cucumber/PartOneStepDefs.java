b52b5a6838a4d63d6385c435ebdb9d95dead1cfd
Navaty Khara <navatykhara@gmail.com>
2022-11-14 19:53:24 -0500

    STEPDEF row45 step definitions

--- src/test/java/Cucumber/PartOneStepDefs.java
+++ src/test/java/Cucumber/PartOneStepDefs.java
@@ -1,22 +1,88 @@
 package Cucumber;
 
+import Dice.*;
+import Game.Player;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PartOneStepDefs {
+
+    Player p1;
+    Scorer scorer;
+
+    //row45
     @Given("I have a hand of {string}")
     public void i_have_a_hand_of(String hand){
+        p1 = new Player(1);
+        stringToHand(hand);
+        System.out.println("I have a hand of " + p1.getHand().toString());
     }
     @When("I die")
     public void i_die(){
+        System.out.println("I die");
+        assertFalse(p1.isAlive());
     }
     @Then("I should get a score of {int}")
     public void i_should_get_a_score_of(Integer int1){
-        System.out.println(int1);
+        scorer = new Scorer();
+        scorer.setAlive(p1.isAlive());
+        scorer.count(p1.getHand());
+        assertEquals(scorer.score(), int1);
+        System.out.println("I should get a score of " + int1);
     }
+
+    public void stringToHand(String hand){
+
+        Dice d1 = new Dice();
+        Dice d2 = new Dice();
+        Dice d3 = new Dice();
+        Dice d4 = new Dice();
+        Dice d5 = new Dice();
+        Dice d6 = new Dice();
+        Dice d7 = new Dice();
+        Dice d8 = new Dice();
+
+        Dice[] dices = new Dice[]{d1, d2, d3, d4, d5, d6, d7, d8};
+
+        String[] cards = hand.split(",");
+
+        for(int i = 0; i < 8; i++){
+            switch(cards[i]){
+                case "DIAMOND":
+                    dices[i].setDice(DiceState.DIAMOND);
+                    break;
+                case "COIN":
+                    dices[i].setDice(DiceState.COIN);
+                    break;
+                case "MONKEY":
+                    dices[i].setDice(DiceState.MONKEY);
+                    break;
+                case "PARROT":
+                    dices[i].setDice(DiceState.PARROT);
+                    break;
+                case "SWORD":
+                    dices[i].setDice(DiceState.SWORD);
+                    break;
+                case "SKULL":
+                    dices[i].setDice(DiceState.SKULL);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        Arrays.sort(dices, new SortHelper());
+
+        p1.getHand().setHand(dices);
+        p1.getHand().cleanUp();
+
+    }
+
+
 
 }
--- src/test/resources/Cucumber/partOne.feature
+++ src/test/resources/Cucumber/partOne.feature
@@ -1,5 +1,4 @@
 Feature: Does single player scoring work?
-  Everybody wants to know if single player scoring works.
 
   Scenario: row45
     Given I have a hand of "SKULL,SKULL,SKULL,SWORD,SWORD,SWORD,SWORD,SWORD"
